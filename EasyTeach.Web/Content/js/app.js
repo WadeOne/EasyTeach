34abf99621cdfe48907270e0ee1af543a6359689
Olga Petkevich <olya2max2010@gmail.com>
2014-04-03 23:18:23 +0300

    Add ajax for user registration

--- EasyTeach.Web/Content/js/app.js
+++ EasyTeach.Web/Content/js/app.js
@@ -1,3 +1,20 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+//$(document).foundation();
+
+$( document ).ready(function() {
+	 $('#create-user-button').on('click', function(event) {
+		  var $form = $('#user-registration-form');
+		  $.ajax({
+		    url: $form.attr('action'),
+		    type: 'POST',
+		    data: $form.serialize(),
+		    traditional: true,
+		    error: function (jqXHR, textStatus, error) {
+		     
+		    },
+		    success: function (data) {
+		    }
+		  });
+	 })
+});

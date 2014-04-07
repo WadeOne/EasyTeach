59aeb039e08080874019f638b5a18d4f28a12339
Irina Chernik <irina.chernik@gmail.com>
2014-04-07 14:37:37 +0300

    Changed Scripts structure

--- EasyTeach.Web/Scripts/vendor/config.rb
+++ EasyTeach.Web/Scripts/vendor/config.rb
@@ -3,10 +3,10 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "../../Content/stylesheets"
+sass_dir = "../../Content/scss"
+images_dir = "../../Content/images"
+javascripts_dir = "../app"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed

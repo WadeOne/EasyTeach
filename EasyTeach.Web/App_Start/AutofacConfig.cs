6445e8f2fd2867e46c8324afef4afacefd4585c2
Ivan Abragimovich <ivan.abragimovich@gmail.com>
2014-04-08 00:22:22 +0300

    Move DI config into separate class

---
+++ EasyTeach.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using EasyTeach.Core.Repositories;
+using EasyTeach.Core.Repositories.Mappers;
+using EasyTeach.Core.Services.UserManagement;
+using EasyTeach.Core.Services.UserManagement.Impl;
+using EasyTeach.Data.Repostitories;
+using EasyTeach.Data.Repostitories.Mappers;
+
+namespace EasyTeach.Web
+{
+    public static class AutofacConfig
+    {
+        public static void RegisterDependencies()
+        {
+            // TODO: add unit test for resolving dependecies
+            var builder = new ContainerBuilder();
+            builder.RegisterType<UserRepository>().As<IUserRepository>();
+            builder.RegisterType<UserService>().As<IUserService>();
+            builder.RegisterType<UserDtoMapper>().As<IUserDtoMapper>();
+        }
+    }
+}
--- EasyTeach.Web/Global.asax.cs
+++ EasyTeach.Web/Global.asax.cs
@@ -3,16 +3,6 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-using Autofac;
-
-using EasyTeach.Core.Entities.Data;
-using EasyTeach.Core.Repositories;
-using EasyTeach.Core.Repositories.Mappers;
-using EasyTeach.Core.Services.UserManagement;
-using EasyTeach.Core.Services.UserManagement.Impl;
-using EasyTeach.Data.Repostitories;
-using EasyTeach.Data.Repostitories.Mappers;
-
 namespace EasyTeach.Web
 {
     public class WebApiApplication : System.Web.HttpApplication
@@ -24,11 +14,7 @@ namespace EasyTeach.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            var builder = new ContainerBuilder();
-            builder.RegisterType<UserRepository>().As<IUserRepository>();
-            builder.RegisterType<UserService>().As<IUserService>();
-            builder.RegisterType<UserDtoMapper>().As<IUserDtoMapper>();
+            AutofacConfig.RegisterDependencies();
         }
     }
 }

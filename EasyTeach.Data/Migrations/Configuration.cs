ba6922318b13cbdeda03843fdc4a74676274d873
Ivan Abragimovich <ivan.abragimovich@gmail.com>
2014-04-08 23:38:35 +0300

    Add Code First migrations
    Good example of CF with EF6:
    http://www.asp.net/mvc/tutorials/getting-started-with-ef-using-mvc/migrations-and-deployment-with-the-entity-framework-in-an-asp-net-mvc-application

---
+++ EasyTeach.Data/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace EasyTeach.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EasyTeach.Data.Context.EasyTeachContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EasyTeach.Data.Context.EasyTeachContext";
+        }
+
+        protected override void Seed(EasyTeach.Data.Context.EasyTeachContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}

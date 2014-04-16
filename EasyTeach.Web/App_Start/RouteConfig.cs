3be13b28d849bae2ebd90dcb669bff960dc07e84
Ivan Abragimovich <ivan.abragimovich@gmail.com>
2014-04-16 13:56:39 +0300

    Fix routes

--- EasyTeach.Web/App_Start/RouteConfig.cs
+++ EasyTeach.Web/App_Start/RouteConfig.cs
@@ -3,19 +3,11 @@ using System.Web.Routing;
 
 namespace EasyTeach.Web
 {
-    public class RouteConfig
+    public static class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.IgnoreRoute("*.html");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
         }
     }
 }

2348cf08dd8d47857edfa6337a7512fe62dc103d
Olga Petkevich <olga.petkevich@hiqo-solutions.com>
2014-04-17 16:00:34 +0300

    Implement render login view

--- EasyTeach.Web/Scripts/app/controllers/home-controller.js
+++ EasyTeach.Web/Scripts/app/controllers/home-controller.js
@@ -1,14 +1,21 @@
 
 define([
-		'controllers/base/controller',
-		'views/login-view',
-		'views/register-view'
-	], function(Controller, LoginView, RegisterView){
-		var HomeController = Controller.extend({
-			login: function(params) {
-				this.view = new LoginView();
-			}
-		})
-		return HomeController;
-
-})
+        'controllers/base/controller',
+        'views/login-view',
+        'views/register-view',
+        'views/site-view'
+    ], function(Controller, LoginView, RegisterView, SiteView) {
+        var HomeController = Controller.extend({
+            beforeAction: function () {
+                this.reuse('site', SiteView);
+                //this.reuse('login', LoginView);
+            },
+            /*renderView: function () {
+                this.view = new SiteView();
+            },*/
+            login: function () {
+                this.view = new LoginView({region: 'main'});
+            }
+        });
+        return HomeController;
+    });
--- EasyTeach.Web/Scripts/app/routes.js
+++ EasyTeach.Web/Scripts/app/routes.js
@@ -3,7 +3,8 @@ define(function() {
 
   // The routes for the application. This module returns a function.
   // `match` is match method of the Router
-  return function(match) {
+  return function (match) {
+    //match('', 'home#renderView');
     match('', 'home#login');
     match('register', 'home#register');
   };

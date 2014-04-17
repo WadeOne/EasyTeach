82290a88c8fa8d50fd96b49f13de8fbc5db76b95
Victor Zhuk <victor.net.su@gmail.com>
2014-04-17 23:27:50 +0300

    Move menu to separate view, implement simple switcher for menu buttons menu still needs in refactor from template to menu collection

--- EasyTeach.Web/Scripts/app/controllers/base/controller.js
+++ EasyTeach.Web/Scripts/app/controllers/base/controller.js
@@ -1,9 +1,14 @@
-define(['chaplin', 'views/site-view'], function(Chaplin, SiteView) {
+define([
+    'chaplin',
+    'views/site-view',
+    'views/shared/menu'
+], function(Chaplin, SiteView, MenuView) {
   'use strict';
 
   var Controller = Chaplin.Controller.extend({
       beforeAction: function () {
           this.reuse('site', SiteView);
+          this.reuse('menu', MenuView);
       }
   });
 

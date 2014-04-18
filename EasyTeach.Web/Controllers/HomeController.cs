9111a641d7aaf5300abfbe998525287a9e357672
Ivan Abragimovich <ivan.abragimovich@gmail.com>
2014-04-18 13:12:53 +0300

    Configure routes to be compatible with backbone routes

---
+++ EasyTeach.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace EasyTeach.Web.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return File("~/index.html", "text/html");
+        }
+    }
+}

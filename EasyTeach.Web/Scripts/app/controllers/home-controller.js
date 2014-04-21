faaa0c7ecf27cf1dfabe3c4690838861aea9392e
Olga Petkevich <olya2max2010@gmail.com>
2014-04-21 22:56:44 +0300

    Add reset password template and view

--- EasyTeach.Web/Scripts/app/controllers/home-controller.js
+++ EasyTeach.Web/Scripts/app/controllers/home-controller.js
@@ -1,14 +1,18 @@
 define([
         'controllers/base/controller',
         'views/login-view',
-        'views/register-view'
-    ], function(Controller, LoginView, RegisterView) {
+        'views/register-view',
+        'views/reset-password-view'
+    ], function(Controller, LoginView, RegisterView, ResetPasswordView) {
         var HomeController = Controller.extend({
             login: function () {
                 this.view = new LoginView({region: 'main'});
             },
             register: function () {
                 this.view = new RegisterView({region: 'main'});
+            },
+            resetPassword: function () {
+                this.view = new ResetPasswordView({ region: 'main' });
             }
         });
     return HomeController;

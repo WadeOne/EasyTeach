d6072d3d5e82d11152a47b6f251b57f35d10f6a1
Artem Prudnikov <wadestillone@gmail.com>
2014-04-23 02:39:12 +0300

    Started implementing QuizDtoMapper

---
+++ EasyTeach.Data.Tests/Repostitories/Mappers/QuizDtoMapperTest.cs
@@ -0,0 +1,21 @@
+﻿using EasyTeach.Data.Repostitories.Mappers;
+using Xunit;
+
+namespace EasyTeach.Data.Tests.Repostitories.Mappers
+{
+    public class QuizDtoMapperTest
+    {
+        private readonly QuizDtoMapper _mapper;
+
+        public QuizDtoMapperTest()
+        {
+            _mapper = new QuizDtoMapper();
+        }
+
+        [Fact]
+        public void Map_FromModelToDto_Mapped()
+        {
+            
+        }
+    }
+}
---
+++ EasyTeach.Data/Repostitories/Mappers/QuizDtoMapper.cs
@@ -0,0 +1,24 @@
+﻿using EasyTeach.Core.Entities.Data;
+using EasyTeach.Core.Entities.Services;
+using EasyTeach.Core.Repositories.Mappers;
+
+namespace EasyTeach.Data.Repostitories.Mappers
+{
+    public class QuizDtoMapper : IQuizDtoMapper
+    {
+        public IQuizDto Map(IQuizModel quiz)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IQuizModel Map(IQuizDto quizDto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IAssignedTestDto Map(IAssignedTestModel assignedTest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

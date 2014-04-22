6b7f4a2a75d8adfd56d402744c8843ec600b6101
Olga Petkevich <olga.petkevich@hiqo-solutions.com>
2014-04-22 11:48:09 +0300

    Add reset password logic

---
+++ EasyTeach.Web/Scripts/app/models/reset-password.js
@@ -0,0 +1,27 @@
+﻿define([
+  'models/base/model',
+  'jquery',
+  'chaplin',
+  'underscore'
+], function (Model, $) {
+    'use strict';
+
+    return Model.extend({
+        defaults: {
+            email: "",
+        },
+        sync: function (method, model) {
+            var self = this;
+            return $.ajax({
+                url: '/api/User/ResetPassword?email=' + model.attributes.email,
+                type: 'POST',
+                data: model.attributes,
+                dataType: 'json'
+            }).success(function (data) {
+                self.trigger("success", data);
+            }).fail(function (errorData) {
+                self.trigger("error", errorData);
+            });
+        }
+    });
+});
--- EasyTeach.Web/Scripts/app/views/reset-password-view.js
+++ EasyTeach.Web/Scripts/app/views/reset-password-view.js
@@ -1,12 +1,33 @@
 ﻿define([
     'views/base/view',
-    'text!templates/reset-password.html'
-], function (View, template) {
+    'text!templates/reset-password.html',
+     'models/reset-password'
+], function (View, template, ResetPasswordModel) {
     'use strict';
 
     return View.extend({
         container: '#content',
         template: template,
-        autoRender: true        
+        autoRender: true,
+        events: {
+            "click #reset-btn": "resetPassword"
+        },
+        resetPassword: function () {
+            var form = this.$('#reset-password-form');
+
+            new ResetPasswordModel({
+                email: form.find('input[name=email]').val()
+            }).on('success', this.resetPasswordSuccess)
+            .on('error', this.resetPasswordFail)
+            .save();
+
+            return false;
+        },
+        resetPasswordSuccess: function (data) {
+            window.alert("success: " + data);
+        },
+        resetPasswordFail: function (errorData) {
+            window.alert(errorData.statusText);
+        }
     });
 });

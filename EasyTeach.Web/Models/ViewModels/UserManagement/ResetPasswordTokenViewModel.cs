ca1f0c67ab2a39457f2023aff537b7a80b085663
Ivan Abragimovich <ivan.abragimovich@gmail.com>
2014-05-16 01:39:34 +0300

    #21 API: on success confirmEmail (api/User/ConfirmEmail) should return empty json object

---
+++ EasyTeach.Web/Models/ViewModels/UserManagement/ResetPasswordTokenViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EasyTeach.Web.Models.ViewModels.UserManagement
+{
+    public sealed class ResetPasswordTokenViewModel
+    {
+         public string ResetPasswordToken { get; set; }
+    }
+}
